8d01858a9fe7a2733da173a549172efd8b16edf6
Dzmitry Khokha <khokha.dmitry@gmail.com>
2022-02-06 00:35:10 +0300

    Make task-1

---
+++ lesson-2-basic-constructions/script.js
@@ -0,0 +1,16 @@
+'use strict'
+
+function findVowels () {
+    var userStr = prompt('Введите строку: ', '');
+    var vowels = ['а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я'];
+    var count = 0;
+
+    for (var char of userStr.toLowerCase()) {
+        if (vowels.includes(char)) {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+console.log('Кол-во гласных букв в строке: ' + findVowels());
